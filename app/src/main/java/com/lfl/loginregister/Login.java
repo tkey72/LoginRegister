18830e704543854019f46652524984d143a8b37b
Dmitry Makeev <dimon_makeev@mail.ru>
2018-05-28 17:47:17 +0300

    Initial commit

---
+++ app/src/main/java/com/lfl/loginregister/Login.java
@@ -0,0 +1,47 @@
+package com.lfl.loginregister;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    Button bLogin;
+    EditText etUsername, etPassword;
+    TextView tvRegisterLink;
+    UserLocalStore userLocalStore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+        bLogin.setOnClickListener(this);
+        tvRegisterLink.setOnClickListener(this);
+        userLocalStore = new UserLocalStore(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bLogin:
+                User user = new User(null, null);
+                userLocalStore.storeUserData(user);
+                userLocalStore.setUserLoggedIn(true);
+
+                break;
+
+            case R.id.tvRegisterLink:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/lfl/loginregister/UserLocalStore.java
@@ -0,0 +1,54 @@
+package com.lfl.loginregister;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class UserLocalStore {
+
+    public static final String SP_NAME = "userDetails";
+    SharedPreferences userLocalDatabase;
+
+    public UserLocalStore(Context context){
+        userLocalDatabase = context.getSharedPreferences(SP_NAME, 0);
+    }
+
+    public void storeUserData(User user){
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putString("name", user.name);
+        spEditor.putInt("age", user.age);
+        spEditor.putString("username", user.username);
+        spEditor.putString("password", user.password);
+        spEditor.commit();
+    }
+
+    public User getLoggedInUser(){
+        String name = userLocalDatabase.getString("name", "");
+        int age = userLocalDatabase.getInt("age", -1);
+        String username = userLocalDatabase.getString("username", "");
+        String password = userLocalDatabase.getString("password", "");
+
+        User storedUser = new User(name, age, username, password);
+        return storedUser;
+    }
+
+    public void setUserLoggedIn(boolean loggedIn){
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("loggedIn", loggedIn);
+        spEditor.commit();
+    }
+
+    public boolean getUserLoggidIn(){
+        if (userLocalDatabase.getBoolean("loggedIn", false) == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void clearUserData(){
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.clear();
+        spEditor.commit();
+    }
+}
